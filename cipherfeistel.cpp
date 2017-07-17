93dd9ffa014834fc56d8318326884ebea07b3669
Maxim <maksimrybak666@gmail.com>
2017-07-17 12:42:28 +0300

    Convert to BITE finish

--- cipherfeistel.cpp
+++ cipherfeistel.cpp
@@ -1,7 +1,21 @@
 #include "cipherfeistel.h"
+#include <vector>
 
 void CipherFeistel::CipherAlgoritm(string str){
     str = Check32Block(str);
+    int i = 0;
+
+    //Convert to Bite string str
+    vector<int> strASCI(str.size());
+    strASCI.assign(&str[0],&str[0]+str.size());
+
+    /*while(strASCI.size != i){
+        for(int j = 0; j<4; j++){
+            left = strASCI
+        }
+        i+=16;r
+    }*/
+
     std::cout<<"I'm work "<<str<<endl;
 };
 
